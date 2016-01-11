1a9c68b67a9092a8cf2f886914b66098f2cbaf85
Ben Redden <benjamminredden@gmail.com>
2016-01-11 07:23:46 -0500

    changes to js n junk

--- js/ratingFunctions.js
+++ js/ratingFunctions.js
@@ -14,8 +14,40 @@ jQuery( document ).ready( function() {
             data: "action=post_up&nonce="+ajax_var.nonce+"&post_id="+post_id,
             success: function( response ) {
                 // if user hasnt voted and there are votes already
-                if( response != "already voted" && response != "0%")
+                if( response != "already voted" && response != "0%" )
                 {
+                    // set up some vars for animations
+                    // var ratePercentage = jQuery( '.post-rate .ratePercentage h2' ),
+                    //     startVal = jQuery( '.post-rate .ratePercentage h2' ).text(),
+                    //     ajaxResponse = response,
+                    //     endVal = parseInt( ajaxResponse.slice( 0, -1 ) ),
+                    //     fontSize = ratePercentage.css( 'font-size' ),
+                    //     currentVal;
+                    //
+                    // currentVal = startVal;
+                    //
+                    // var upOrDown = setInterval( function ()
+                    // {
+                    //     // if they're the same
+                    //     if ( currentVal === endVal )
+                    //     {
+                    //         clearInterval( upOrDown );
+                    //         ratePercentage.animate( { fontSize: fontSize }, 100 );
+                    //     }
+                    //     // if currentVal is larger
+                    //     else if ( currentVal > endVal )
+                    //     {
+                    //         currentVal++;
+                    //         ratePercentage.html( currentVal + '%' ).animate( { fontSize: '+=.25' }, 100 );
+                    //     }
+                    //     // if endVal is larger
+                    //     else if ( currentVal < endVal )
+                    //     {
+                    //         currentVal--;
+                    //         ratePercentage.html( currentVal + '%' ).animate( { fontSize: '+=.25' }, 100 );
+                    //     }
+                    // }, 100 );
+
                     // add the percentage
                     jQuery( '.post-rate .ratePercentage h2' ).html( response );
                     jQuery( '.post-rate .ratePercentage .percentageChart' ).animate({
@@ -66,7 +98,7 @@ jQuery( document ).ready( function() {
             // If vote successful
             success: function( response ) {
                 // if user hasnt voted and there are votes already
-                if( response != "already voted" && response != "0%")
+                if( response != "already voted" && response != "0%" )
                 {
                     // add the percentage
                     jQuery( '.post-rate .ratePercentage h2' ).html( response );
